be40bc0d054aa03445845491e5af86e71805c246
Carlos Vigil <carlos.vigil@icloud.com>
2016-11-07 21:47:20 -0500

    complete Food Xcode app part I

---
+++ Mobile I/FoodTracker - Build a Basic UI/FoodTracker/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 5/23/15.
+//  Copyright © 2015 Apple Inc. All rights reserved.
+//  See LICENSE.txt for this sample’s licensing information.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
+    }
+
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+
+}
+
